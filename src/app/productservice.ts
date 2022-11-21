8daf09fdf02fa66dadbfc0a230d5188da9a882dc
Ramesh <ramesh.thumati@odinmo.com>
2022-11-21 11:06:40 +0530

    first commit

---
+++ src/app/productservice.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Product } from './product';
+
+@Injectable()
+export class ProductService {
+    constructor(private http: HttpClient) { }
+
+    getProducts() {
+        return this.http.get<any>('assets/products.json')
+        .toPromise()
+        .then(res => <Product[]>res.data)
+        .then(data => { return data; });
+    }
+}
